3c19c88be500107c090a70fcc1634b24f70cdc1e
JeffreyLiangUF <jeffreyliang@ufl.edu>
2019-11-06 00:28:33 -0500

    css edits

--- bc4/src/components/ViewBuilding.js
+++ bc4/src/components/ViewBuilding.js
@@ -3,25 +3,25 @@ import React from 'react';
 class ViewBuilding extends React.Component {
 	render() {
 		const {data, selectedBuilding, selected} = this.props
+		var longitude;
+		var latitude;
 		const buildingInfo = data
 		.filter(building => {return building.id === selectedBuilding})
-		.map(listing => {
-			var longitude = null
-			var latitude = null
-			if(listing.coordinates == null){
+		.map(listInfo => {
+			if(listInfo.coordinates == null){
 				latitude = 'N/A'
 				longitude = 'N/A'
 			}
 			else {
-				longitude = listing.coordinates.longitude
-				latitude = listing.coordinates.latitude
+				longitude = listInfo.coordinates.longitude
+				latitude = listInfo.coordinates.latitude
 			}
 
 			return (
-				<ul key={listing.id}>Coordinates: 
-					<br/> Longitude: {longitude} 
-					<br/> Latitude: {latitude} 
-					<br/> Address: {listing.address}
+				<ul key={listInfo.id}>Coordinates: 
+					<br/> <span className="tab">Longitude: {longitude} </span>
+					<br/> <span className="tab">Latitude: {latitude} </span> 
+					<br/> Address: {listInfo.address}
 				</ul>
 			);
 		});
